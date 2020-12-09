4069de4c0653181619ec2ac3381e0d466cd98cdd
simon <sjuresa@gmail.com>
2020-12-09 20:59:04 +0100

    Add license and readme

--- src/main/java/nomblox/FatalityListener.java
+++ src/main/java/nomblox/FatalityListener.java
@@ -1,3 +1,21 @@
+/*
+ * Fatality
+ * Copyright (C) 2020 Nejc Korošec and Simon Jureša
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package nomblox;
 
 import org.bukkit.Location;
@@ -11,7 +29,7 @@ import org.bukkit.event.entity.EntityDeathEvent;
 public class FatalityListener implements Listener {
     @EventHandler
     public void onEntityDeath(EntityDeathEvent entityDeathEvent) {
-        if (entityDeathEvent.getEntity().getKiller() instanceof Player) {
+        if (entityDeathEvent.getEntity().getKiller() != null) {
             Player player = entityDeathEvent.getEntity().getKiller();
             if (player.hasPermission("fatality.blood")) {
                 Location location = entityDeathEvent.getEntity().getLocation().add(0, 1, 0);
